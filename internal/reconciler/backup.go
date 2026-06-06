0c591eeaeb4d181f06d6ebf446fa034378171fb5
AlienAscension <linus.breitenberger@gmail.com>
2026-06-06 14:38:11 +0200

    feat: add backup reconciler builder functions and tests

---
+++ internal/reconciler/backup.go
@@ -0,0 +1,305 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reconciler
+
+import (
+	"fmt"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+
+	gamesv1alpha1 "github.com/gobehost/operator/api/v1alpha1"
+)
+
+type BackupConfig struct {
+	Endpoint        string
+	Bucket          string
+	Path            string
+	SecretName      string
+	SecretNamespace string
+}
+
+func ResolveStorageConfig(backup *gamesv1alpha1.GameServerBackup, platformConfig *BackupConfig) *BackupConfig {
+	cfg := &BackupConfig{
+		Endpoint:        platformConfig.Endpoint,
+		Bucket:          platformConfig.Bucket,
+		Path:            platformConfig.Path,
+		SecretName:      platformConfig.SecretName,
+		SecretNamespace: platformConfig.SecretNamespace,
+	}
+
+	if backup.Spec.Storage != nil {
+		storage := backup.Spec.Storage
+		if storage.Endpoint != "" {
+			cfg.Endpoint = storage.Endpoint
+		}
+		if storage.Bucket != "" {
+			cfg.Bucket = storage.Bucket
+		}
+		if storage.Path != "" {
+			cfg.Path = storage.Path
+		}
+		if storage.SecretRef != nil && storage.SecretRef.Name != "" {
+			cfg.SecretName = storage.SecretRef.Name
+		}
+	}
+
+	if cfg.Path == "" {
+		cfg.Path = backup.Namespace + "/" + backup.Spec.TargetRef.Name
+	}
+
+	cfg.SecretNamespace = backup.Namespace
+
+	return cfg
+}
+
+func BuildCronJob(backup *gamesv1alpha1.GameServerBackup, cfg *BackupConfig, targetKind, targetName, pvcName string) *batchv1.CronJob {
+	env := []corev1.EnvVar{
+		{Name: "RCLONE_S3_PROVIDER", Value: "Other"},
+		{Name: "RCLONE_S3_ENDPOINT", Value: cfg.Endpoint},
+		{Name: "RCLONE_S3_ACCESS_KEY_ID", ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: cfg.SecretName,
+				},
+				Key: "accessKeyId",
+			},
+		}},
+		{Name: "RCLONE_S3_SECRET_ACCESS_KEY", ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: cfg.SecretName,
+				},
+				Key: "secretAccessKey",
+			},
+		}},
+		{Name: "BACKUP_PATH", Value: cfg.Path},
+		{Name: "BACKUP_BUCKET", Value: cfg.Bucket},
+	}
+
+	retention := int32(5)
+	if backup.Spec.Retention != nil {
+		retention = *backup.Spec.Retention
+	}
+	env = append(env, corev1.EnvVar{
+		Name:  "BACKUP_RETENTION",
+		Value: fmtInt32(retention),
+	})
+
+	if backup.Spec.IncludeMetadata != nil {
+		env = append(env, corev1.EnvVar{
+			Name:  "INCLUDE_METADATA",
+			Value: fmtBool(*backup.Spec.IncludeMetadata),
+		})
+	} else {
+		env = append(env, corev1.EnvVar{
+			Name:  "INCLUDE_METADATA",
+			Value: "true",
+		})
+	}
+
+	volumeName := "data"
+	container := corev1.Container{
+		Name:    "backup",
+		Image:   "rclone/rclone:latest",
+		Command: []string{"/bin/sh", "-c"},
+		Args: []string{
+			"/usr/bin/rclone copy /data $RCLONE_REMOTE/$BACKUP_BUCKET/$BACKUP_PATH/$(date -u +%Y-%m-%dT%H-%M-%SZ)/",
+		},
+		Env: env,
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      volumeName,
+				MountPath: "/data",
+				ReadOnly:  true,
+			},
+		},
+	}
+
+	podSpec := corev1.PodSpec{
+		Containers:    []corev1.Container{container},
+		RestartPolicy: corev1.RestartPolicyOnFailure,
+		Volumes: []corev1.Volume{
+			{
+				Name: volumeName,
+				VolumeSource: corev1.VolumeSource{
+					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+						ClaimName: pvcName,
+						ReadOnly:  true,
+					},
+				},
+			},
+		},
+	}
+
+	labels := GameServerBackupLabels(backup)
+
+	return &batchv1.CronJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      backup.Name + "-backup",
+			Namespace: backup.Namespace,
+			Labels:    labels,
+		},
+		Spec: batchv1.CronJobSpec{
+			Schedule:                   backup.Spec.Schedule,
+			SuccessfulJobsHistoryLimit: ptr.To(int32(3)),
+			FailedJobsHistoryLimit:     ptr.To(int32(3)),
+			JobTemplate: batchv1.JobTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: batchv1.JobSpec{
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: labels,
+						},
+						Spec: podSpec,
+					},
+				},
+			},
+		},
+	}
+}
+
+func BuildBackupOnDeleteJob(backup *gamesv1alpha1.GameServerBackup, cfg *BackupConfig, targetKind, targetName, pvcName string) *batchv1.Job {
+	env := []corev1.EnvVar{
+		{Name: "RCLONE_S3_PROVIDER", Value: "Other"},
+		{Name: "RCLONE_S3_ENDPOINT", Value: cfg.Endpoint},
+		{Name: "RCLONE_S3_ACCESS_KEY_ID", ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: cfg.SecretName,
+				},
+				Key: "accessKeyId",
+			},
+		}},
+		{Name: "RCLONE_S3_SECRET_ACCESS_KEY", ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: cfg.SecretName,
+				},
+				Key: "secretAccessKey",
+			},
+		}},
+		{Name: "BACKUP_PATH", Value: cfg.Path},
+		{Name: "BACKUP_BUCKET", Value: cfg.Bucket},
+	}
+
+	retention := int32(5)
+	if backup.Spec.Retention != nil {
+		retention = *backup.Spec.Retention
+	}
+	env = append(env, corev1.EnvVar{
+		Name:  "BACKUP_RETENTION",
+		Value: fmtInt32(retention),
+	})
+
+	if backup.Spec.IncludeMetadata != nil {
+		env = append(env, corev1.EnvVar{
+			Name:  "INCLUDE_METADATA",
+			Value: fmtBool(*backup.Spec.IncludeMetadata),
+		})
+	} else {
+		env = append(env, corev1.EnvVar{
+			Name:  "INCLUDE_METADATA",
+			Value: "true",
+		})
+	}
+
+	volumeName := "data"
+	container := corev1.Container{
+		Name:    "backup",
+		Image:   "rclone/rclone:latest",
+		Command: []string{"/bin/sh", "-c"},
+		Args: []string{
+			"/usr/bin/rclone copy /data $RCLONE_REMOTE/$BACKUP_BUCKET/$BACKUP_PATH/$(date -u +%Y-%m-%dT%H-%M-%SZ)/",
+		},
+		Env: env,
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      volumeName,
+				MountPath: "/data",
+				ReadOnly:  true,
+			},
+		},
+	}
+
+	podSpec := corev1.PodSpec{
+		Containers:    []corev1.Container{container},
+		RestartPolicy: corev1.RestartPolicyOnFailure,
+		Volumes: []corev1.Volume{
+			{
+				Name: volumeName,
+				VolumeSource: corev1.VolumeSource{
+					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+						ClaimName: pvcName,
+						ReadOnly:  true,
+					},
+				},
+			},
+		},
+	}
+
+	labels := GameServerBackupLabels(backup)
+
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      backup.Name + "-backup-on-delete",
+			Namespace: backup.Namespace,
+			Labels:    labels,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: podSpec,
+			},
+		},
+	}
+}
+
+func GameServerBackupLabels(backup *gamesv1alpha1.GameServerBackup) map[string]string {
+	return map[string]string{
+		"app.kubernetes.io/name":       "gameserverbackup",
+		"app.kubernetes.io/instance":   backup.Name,
+		"app.kubernetes.io/managed-by": "gobehost-operator",
+		"games.gobehost.com/backup":    backup.Name,
+	}
+}
+
+func GetPVCName(targetKind, targetName string) string {
+	switch targetKind {
+	case "GameServer":
+		return "data-" + targetName + "-0"
+	default:
+		return ""
+	}
+}
+
+func fmtInt32(v int32) string {
+	return fmt.Sprintf("%d", v)
+}
+
+func fmtBool(v bool) string {
+	if v {
+		return "true"
+	}
+	return "false"
+}
---
+++ internal/reconciler/backup_test.go
@@ -0,0 +1,336 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reconciler
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+
+	gamesv1alpha1 "github.com/gobehost/operator/api/v1alpha1"
+)
+
+func makeTestBackup() *gamesv1alpha1.GameServerBackup {
+	return &gamesv1alpha1.GameServerBackup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-backup",
+			Namespace: "test-ns",
+		},
+		Spec: gamesv1alpha1.GameServerBackupSpec{
+			TargetRef: gamesv1alpha1.TargetReference{
+				Kind: "GameServer",
+				Name: "my-server",
+			},
+			Schedule:        "0 */6 * * *",
+			Retention:       ptr.To(int32(5)),
+			IncludeMetadata: ptr.To(true),
+			BackupOnDelete:  ptr.To(true),
+		},
+	}
+}
+
+func makePlatformConfig() *BackupConfig {
+	return &BackupConfig{
+		Endpoint:        "https://s3.example.com",
+		Bucket:          "platform-bucket",
+		Path:            "platform/path",
+		SecretName:      "platform-secret",
+		SecretNamespace: "platform-ns",
+	}
+}
+
+var _ = Describe("ResolveStorageConfig", func() {
+	It("should use platform defaults when spec storage is nil", func() {
+		backup := makeTestBackup()
+		backup.Spec.Storage = nil
+		cfg := ResolveStorageConfig(backup, makePlatformConfig())
+		Expect(cfg.Endpoint).To(Equal("https://s3.example.com"))
+		Expect(cfg.Bucket).To(Equal("platform-bucket"))
+		Expect(cfg.Path).To(Equal("platform/path"))
+		Expect(cfg.SecretName).To(Equal("platform-secret"))
+	})
+
+	It("should override platform defaults with spec values", func() {
+		backup := makeTestBackup()
+		backup.Spec.Storage = &gamesv1alpha1.BackupStorageSpec{
+			Endpoint: "https://custom-s3.example.com",
+			Bucket:   "custom-bucket",
+			Path:     "custom/path",
+			SecretRef: &gamesv1alpha1.BackupSecretRef{
+				Name: "custom-secret",
+			},
+		}
+		cfg := ResolveStorageConfig(backup, makePlatformConfig())
+		Expect(cfg.Endpoint).To(Equal("https://custom-s3.example.com"))
+		Expect(cfg.Bucket).To(Equal("custom-bucket"))
+		Expect(cfg.Path).To(Equal("custom/path"))
+		Expect(cfg.SecretName).To(Equal("custom-secret"))
+	})
+
+	It("should use platform defaults for fields not overridden", func() {
+		backup := makeTestBackup()
+		backup.Spec.Storage = &gamesv1alpha1.BackupStorageSpec{
+			Bucket: "custom-bucket",
+		}
+		cfg := ResolveStorageConfig(backup, makePlatformConfig())
+		Expect(cfg.Endpoint).To(Equal("https://s3.example.com"))
+		Expect(cfg.Bucket).To(Equal("custom-bucket"))
+		Expect(cfg.Path).To(Equal("platform/path"))
+		Expect(cfg.SecretName).To(Equal("platform-secret"))
+	})
+
+	It("should compute default path from namespace/target-name when path is empty", func() {
+		backup := makeTestBackup()
+		backup.Spec.Storage = nil
+		platformCfg := &BackupConfig{
+			Endpoint:        "https://s3.example.com",
+			Bucket:          "platform-bucket",
+			Path:            "",
+			SecretName:      "platform-secret",
+			SecretNamespace: "platform-ns",
+		}
+		cfg := ResolveStorageConfig(backup, platformCfg)
+		Expect(cfg.Path).To(Equal("test-ns/my-server"))
+	})
+
+	It("should always set SecretNamespace to backup namespace", func() {
+		backup := makeTestBackup()
+		cfg := ResolveStorageConfig(backup, makePlatformConfig())
+		Expect(cfg.SecretNamespace).To(Equal("test-ns"))
+	})
+
+	It("should handle empty storage spec with overrides", func() {
+		backup := makeTestBackup()
+		backup.Spec.Storage = &gamesv1alpha1.BackupStorageSpec{}
+		cfg := ResolveStorageConfig(backup, makePlatformConfig())
+		Expect(cfg.Endpoint).To(Equal("https://s3.example.com"))
+		Expect(cfg.Bucket).To(Equal("platform-bucket"))
+		Expect(cfg.SecretName).To(Equal("platform-secret"))
+	})
+})
+
+var _ = Describe("BuildCronJob", func() {
+	var (
+		backup *gamesv1alpha1.GameServerBackup
+		cfg    *BackupConfig
+	)
+
+	BeforeEach(func() {
+		backup = makeTestBackup()
+		cfg = &BackupConfig{
+			Endpoint:        "https://s3.example.com",
+			Bucket:          "my-bucket",
+			Path:            "ns/server",
+			SecretName:      "s3-creds",
+			SecretNamespace: "test-ns",
+		}
+	})
+
+	It("should set correct name and namespace", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(cj.Name).To(Equal("my-backup-backup"))
+		Expect(cj.Namespace).To(Equal("test-ns"))
+	})
+
+	It("should set schedule from spec", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(cj.Spec.Schedule).To(Equal("0 */6 * * *"))
+	})
+
+	It("should set history limits", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(*cj.Spec.SuccessfulJobsHistoryLimit).To(Equal(int32(3)))
+		Expect(*cj.Spec.FailedJobsHistoryLimit).To(Equal(int32(3)))
+	})
+
+	It("should set labels", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(cj.Labels["games.gobehost.com/backup"]).To(Equal("my-backup"))
+		Expect(cj.Labels["app.kubernetes.io/managed-by"]).To(Equal("gobehost-operator"))
+	})
+
+	It("should configure container with rclone image", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(container.Name).To(Equal("backup"))
+		Expect(container.Image).To(Equal("rclone/rclone:latest"))
+	})
+
+	It("should set S3 env vars from config and secret refs", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(envValue(container.Env, "RCLONE_S3_PROVIDER")).To(Equal("Other"))
+		Expect(envValue(container.Env, "RCLONE_S3_ENDPOINT")).To(Equal("https://s3.example.com"))
+		Expect(envValue(container.Env, "BACKUP_PATH")).To(Equal("ns/server"))
+		Expect(envValue(container.Env, "BACKUP_BUCKET")).To(Equal("my-bucket"))
+		Expect(envValue(container.Env, "BACKUP_RETENTION")).To(Equal("5"))
+		Expect(envValue(container.Env, "INCLUDE_METADATA")).To(Equal("true"))
+	})
+
+	It("should set secret key refs for S3 credentials", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		secretRef := findEnvVar(container.Env, "RCLONE_S3_ACCESS_KEY_ID")
+		Expect(secretRef).NotTo(BeNil())
+		Expect(secretRef.ValueFrom.SecretKeyRef.Name).To(Equal("s3-creds"))
+		Expect(secretRef.ValueFrom.SecretKeyRef.Key).To(Equal("accessKeyId"))
+	})
+
+	It("should mount PVC read-only at /data", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(container.VolumeMounts).To(HaveLen(1))
+		Expect(container.VolumeMounts[0].Name).To(Equal("data"))
+		Expect(container.VolumeMounts[0].MountPath).To(Equal("/data"))
+		Expect(container.VolumeMounts[0].ReadOnly).To(BeTrue())
+	})
+
+	It("should set volume to reference the PVC", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		volumes := cj.Spec.JobTemplate.Spec.Template.Spec.Volumes
+		Expect(volumes).To(HaveLen(1))
+		Expect(volumes[0].Name).To(Equal("data"))
+		Expect(volumes[0].PersistentVolumeClaim).NotTo(BeNil())
+		Expect(volumes[0].PersistentVolumeClaim.ClaimName).To(Equal("data-my-server-0"))
+		Expect(volumes[0].PersistentVolumeClaim.ReadOnly).To(BeTrue())
+	})
+
+	It("should use custom retention from spec", func() {
+		backup.Spec.Retention = ptr.To(int32(10))
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(envValue(container.Env, "BACKUP_RETENTION")).To(Equal("10"))
+	})
+
+	It("should default includeMetadata to true when nil", func() {
+		backup.Spec.IncludeMetadata = nil
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(envValue(container.Env, "INCLUDE_METADATA")).To(Equal("true"))
+	})
+
+	It("should set includeMetadata to false when spec says false", func() {
+		backup.Spec.IncludeMetadata = ptr.To(false)
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := cj.Spec.JobTemplate.Spec.Template.Spec.Containers[0]
+		Expect(envValue(container.Env, "INCLUDE_METADATA")).To(Equal("false"))
+	})
+
+	It("should set restart policy to OnFailure", func() {
+		cj := BuildCronJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(cj.Spec.JobTemplate.Spec.Template.Spec.RestartPolicy).To(Equal(corev1.RestartPolicyOnFailure))
+	})
+})
+
+var _ = Describe("BuildBackupOnDeleteJob", func() {
+	var (
+		backup *gamesv1alpha1.GameServerBackup
+		cfg    *BackupConfig
+	)
+
+	BeforeEach(func() {
+		backup = makeTestBackup()
+		cfg = &BackupConfig{
+			Endpoint:        "https://s3.example.com",
+			Bucket:          "my-bucket",
+			Path:            "ns/server",
+			SecretName:      "s3-creds",
+			SecretNamespace: "test-ns",
+		}
+	})
+
+	It("should set correct name and namespace", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(job.Name).To(Equal("my-backup-backup-on-delete"))
+		Expect(job.Namespace).To(Equal("test-ns"))
+	})
+
+	It("should set labels", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(job.Labels["games.gobehost.com/backup"]).To(Equal("my-backup"))
+		Expect(job.Labels["app.kubernetes.io/managed-by"]).To(Equal("gobehost-operator"))
+	})
+
+	It("should configure container with same spec as CronJob", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := job.Spec.Template.Spec.Containers[0]
+		Expect(container.Name).To(Equal("backup"))
+		Expect(container.Image).To(Equal("rclone/rclone:latest"))
+		Expect(envValue(container.Env, "RCLONE_S3_PROVIDER")).To(Equal("Other"))
+		Expect(envValue(container.Env, "RCLONE_S3_ENDPOINT")).To(Equal("https://s3.example.com"))
+		Expect(envValue(container.Env, "BACKUP_PATH")).To(Equal("ns/server"))
+		Expect(envValue(container.Env, "BACKUP_BUCKET")).To(Equal("my-bucket"))
+	})
+
+	It("should mount PVC read-only at /data", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		container := job.Spec.Template.Spec.Containers[0]
+		Expect(container.VolumeMounts).To(HaveLen(1))
+		Expect(container.VolumeMounts[0].Name).To(Equal("data"))
+		Expect(container.VolumeMounts[0].MountPath).To(Equal("/data"))
+		Expect(container.VolumeMounts[0].ReadOnly).To(BeTrue())
+	})
+
+	It("should set volume to reference the PVC", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		volumes := job.Spec.Template.Spec.Volumes
+		Expect(volumes).To(HaveLen(1))
+		Expect(volumes[0].PersistentVolumeClaim.ClaimName).To(Equal("data-my-server-0"))
+	})
+
+	It("should set restart policy to OnFailure", func() {
+		job := BuildBackupOnDeleteJob(backup, cfg, "GameServer", "my-server", "data-my-server-0")
+		Expect(job.Spec.Template.Spec.RestartPolicy).To(Equal(corev1.RestartPolicyOnFailure))
+	})
+})
+
+var _ = Describe("GameServerBackupLabels", func() {
+	It("should return correct labels", func() {
+		backup := makeTestBackup()
+		labels := GameServerBackupLabels(backup)
+		Expect(labels["app.kubernetes.io/name"]).To(Equal("gameserverbackup"))
+		Expect(labels["app.kubernetes.io/instance"]).To(Equal("my-backup"))
+		Expect(labels["app.kubernetes.io/managed-by"]).To(Equal("gobehost-operator"))
+		Expect(labels["games.gobehost.com/backup"]).To(Equal("my-backup"))
+	})
+})
+
+var _ = Describe("GetPVCName", func() {
+	It("should return correct PVC name for GameServer", func() {
+		Expect(GetPVCName("GameServer", "my-server")).To(Equal("data-my-server-0"))
+	})
+
+	It("should return empty string for GameServerFleet", func() {
+		Expect(GetPVCName("GameServerFleet", "my-fleet")).To(Equal(""))
+	})
+
+	It("should return empty string for unknown kind", func() {
+		Expect(GetPVCName("Unknown", "something")).To(Equal(""))
+	})
+})
+
+func findEnvVar(env []corev1.EnvVar, name string) *corev1.EnvVar {
+	for i := range env {
+		if env[i].Name == name {
+			return &env[i]
+		}
+	}
+	return nil
+}
